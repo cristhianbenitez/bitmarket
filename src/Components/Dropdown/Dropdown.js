9accbac680d3bcc0d21c83e80f0dfe06da82c6dc
Cristhian Benitez <cristhiantilleria@gmail.com>
2021-11-10 18:35:33 -0500

    As a user, I can select currencies on a dropdown menu

---
+++ src/Components/Dropdown/Dropdown.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import {
+  CurrencyIcon,
+  DropDownContainer,
+  DropDownHeader,
+  DropDownList,
+  ListItem,
+  ListOption
+} from './Dropdown.styles';
+
+export const Dropdown = ({ title, items = [] }) => {
+  const [open, setOpen] = useState(false);
+  const [selection, setSelection] = useState(title);
+
+  const toggle = () => setOpen(!open);
+
+  const handleOnClick = (item) => {
+    setSelection(item.value);
+    toggle(!open);
+  };
+  return (
+    <DropDownContainer>
+      <DropDownHeader
+        tabIndex={0}
+        role="button"
+        onKeyPress={() => {
+          toggle(!open);
+        }}
+        onClick={() => {
+          toggle(!open);
+        }}
+      >
+        <CurrencyIcon /> {selection}
+      </DropDownHeader>
+      {open && (
+        <DropDownList>
+          {items.map((item) => (
+            <ListItem key={item.id} onClick={() => handleOnClick(item)}>
+              <span>{item.value}</span>
+            </ListItem>
+          ))}
+        </DropDownList>
+      )}
+    </DropDownContainer>
+  );
+};
