57d0a7cdfcfa4934f6381a6c236ea9efab5ac70c
Cristhian Benitez <cristhiantilleria@gmail.com>
2021-11-13 09:21:30 -0500

    CA-36 Setup Routes Home and Portfolio

--- src/App.styles.js
+++ src/App.styles.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 import ThemeIcon from './Assets/ThemeIcon.svg';
+import { Link } from 'react-router-dom';
 
 export const Wrapper = styled.div`
   text-align: center;
 `;
-export const NavContainer = styled.nav`
+export const Navbar = styled.nav`
   padding: 0.5em 1.5em;
   display: flex;
   justify-content: space-between;
@@ -16,13 +17,14 @@ export const Container = styled.div`
   display: flex;
 `;
 
-export const StyledButton = styled.a`
+export const StyledButton = styled(Link)`
   font-size: 1.35rem;
   line-height: 30px;
   padding: 0.5em 2em;
   margin-right: 0.5em;
   border-radius: 6px;
-  cursor: pointer;
+  text-decoration: none;
+  color: #fff;
   &:hover,
   &:focus {
     background: #2c2f36;
