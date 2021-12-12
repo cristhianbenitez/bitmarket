9cd67dc22527aad2ca6aa38d1f8876a6211d1d34
Cristhian Benitez <cristhiantilleria@gmail.com>
2021-12-12 15:01:14 -0500

    solved problems with duplicate folders, issues with case sensivity.

--- src/Pages/index.js
+++
@@ -1,3 +0,0 @@
-export { Coins } from './Coins/Coins';
-export { Portfolio } from './Portfolio/Portfolio';
-export { WrappedCoinInformation as CoinInformation } from './CoinInformation/CoinInformation';
