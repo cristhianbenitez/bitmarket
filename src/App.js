86f76dbddc66f1c33e83bc49f0f85b68433d4b37
Cristhian Benitez <cristhiantilleria@gmail.com>
2022-01-02 14:03:57 -0500

    theme provider setup

--- src/App.js
+++ src/App.js
@@ -2,32 +2,37 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Wrapper } from './App.styles';
 import { Coins, Portfolio, CoinInformation } from 'pages';
-import { SubNavbar, Navbar } from 'components';
-
+import { SubNavbar, Navbar, themes } from 'components';
+import { ThemeProvider } from 'styled-components';
 class App extends Component {
   state = {
-    currency: 'USD'
+    currency: 'USD',
+    currentTheme: 'dark'
   };
   changeCurrency = (newCurr) => {
     this.setState({ ...this.state, currency: newCurr });
   };
 
   render() {
+    const theme =
+      this.state.currentTheme === 'dark' ? themes.darkTheme : themes.lightTheme;
     return (
-      <Wrapper onScroll={this.handleScroll}>
-        <Router>
-          <Navbar changeCurrency={this.changeCurrency} />
-          <SubNavbar />
-          <Routes>
-            <Route path="/portfolio" element={<Portfolio />}></Route>
-            <Route
-              path="/"
-              element={<Coins currency={this.state.currency} />}
-            />
-            <Route path="/coin/:id" element={<CoinInformation />} />
-          </Routes>
-        </Router>
-      </Wrapper>
+      <ThemeProvider theme={theme}>
+        <Wrapper onScroll={this.handleScroll}>
+          <Router>
+            <Navbar changeCurrency={this.changeCurrency} />
+            <SubNavbar />
+            <Routes>
+              <Route path="/portfolio" element={<Portfolio />}></Route>
+              <Route
+                path="/"
+                element={<Coins currency={this.state.currency} />}
+              />
+              <Route path="/coin/:id" element={<CoinInformation />} />
+            </Routes>
+          </Router>
+        </Wrapper>
+      </ThemeProvider>
     );
   }
 }
---
+++ src/components/Themes/Themes.js
@@ -0,0 +1,17 @@
+export const darkTheme = {
+  colors: {
+    blackRussian: '#1F2128',
+    persianGreen: '#06B49A',
+    lightBlue: '#AFDBD2',
+    onyx: '#36313D'
+  }
+};
+
+export const lightTheme = {
+  colors: {
+    blackRussian: '#1F2128',
+    persianGreen: '#06B49A',
+    lightBlue: '#AFDBD2',
+    onyx: '#36313D'
+  }
+};
