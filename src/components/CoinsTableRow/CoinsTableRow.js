95a911647b6efd242ba1d467fb2cd706755589f7
Cristhian Benitez <cristhiantilleria@gmail.com>
2022-01-12 08:07:29 -0500

    portfoliopage to functional (#26)
    * refactor coins page and child components
    
    * refactor portfolio page and child components

--- src/components/CoinsTableRow/CoinsTableRow.js
+++ src/components/CoinsTableRow/CoinsTableRow.js
@@ -1,8 +1,9 @@
-import React, { Component, createRef } from 'react';
+import React, { useRef } from 'react';
 import {
   calculatePercentage,
   displayPositiveNumber,
-  formattedNumber
+  formattedNumber,
+  percentageBarColors
 } from 'utils';
 import { Charts } from 'components';
 import {
@@ -25,164 +26,137 @@ import {
   Symbol
 } from './CoinsTableRow.styles';
 import getSymbolFromCurrency from 'currency-symbol-map';
-export class CoinsTableRow extends Component {
-  observer = createRef();
 
-  render() {
-    return this.props.coinItemData.map((coinInfo, index) => {
-      const {
-        name,
-        id,
-        image,
-        symbol,
-        current_price,
-        market_cap,
-        total_volume,
-        circulating_supply,
-        total_supply,
-        price_change_percentage_1h_in_currency: hourlyChanges,
-        price_change_percentage_24h_in_currency: dailyChanges,
-        price_change_percentage_7d_in_currency: weeklyChanges,
-        sparkline_in_7d: pricesOfLastSevenDays
-      } = coinInfo;
+export const CoinsTableRow = (props) => {
+  const observer = useRef();
 
-      const percentageBarColors = {
-        left: [
-          '#FFB528',
-          '#474C77',
-          '#1BA27A',
-          '#BB9F33',
-          '#FE7D43',
-          '#B3404A',
-          '#2775C9',
-          '#83808B',
-          '#345D9D',
-          '#A76558'
-        ],
-        right: [
-          '#FEE158',
-          '#8A92B2',
-          '#FFFFFF',
-          '#E4CD82',
-          '#FFDCCE',
-          '#F4B2B0',
-          '#FFFFFF',
-          '#F09242',
-          '#FFFFFF',
-          '#728D86'
-        ]
-      };
-      const leftSideColors =
-        percentageBarColors.left[index % percentageBarColors.left.length];
+  return props.coinItemData.map((coinInfo, index) => {
+    const {
+      name,
+      id,
+      image,
+      symbol,
+      current_price,
+      market_cap,
+      total_volume,
+      circulating_supply,
+      total_supply,
+      price_change_percentage_1h_in_currency: hourlyChanges,
+      price_change_percentage_24h_in_currency: dailyChanges,
+      price_change_percentage_7d_in_currency: weeklyChanges,
+      sparkline_in_7d: pricesOfLastSevenDays
+    } = coinInfo;
 
-      const rightSideColors =
-        percentageBarColors.right[index % percentageBarColors.right.length];
+    const leftSideColors =
+      percentageBarColors.left[index % percentageBarColors.left.length];
 
-      const currencySymbol = getSymbolFromCurrency(this.props.currency);
-      const minimizedImage = image.replace('large', 'small');
+    const rightSideColors =
+      percentageBarColors.right[index % percentageBarColors.right.length];
 
-      return (
-        <TableRow
-          key={index}
-          ref={(node) => {
-            this.props.lastListElementRef(node, this.observer);
-          }}
-        >
-          <TableData>{index + 1}</TableData>
-          <TableData>
-            <StyledLink to={`coin/${id}`}>
-              <NameContainer>
-                <CoinIcon image={minimizedImage} name={name} />
-                <CoinName>
-                  {name}
-                  <Symbol>({symbol})</Symbol>
-                </CoinName>
-              </NameContainer>
-            </StyledLink>
-          </TableData>
-          <TableData>
-            {currencySymbol}
-            {current_price}
-          </TableData>
-          <PriceChangePercentage price={hourlyChanges}>
-            <CenterDiv>
-              <ArrowIcon price={hourlyChanges} />
-              {displayPositiveNumber(hourlyChanges)}%
-            </CenterDiv>
-          </PriceChangePercentage>
-          <PriceChangePercentage price={dailyChanges}>
-            <CenterDiv>
-              <ArrowIcon price={dailyChanges} />
-              {displayPositiveNumber(dailyChanges)}%
-            </CenterDiv>
-          </PriceChangePercentage>
-          <PriceChangePercentage price={weeklyChanges}>
-            <CenterDiv>
-              <ArrowIcon price={weeklyChanges} />
-              {displayPositiveNumber(weeklyChanges)}%
-            </CenterDiv>
-          </PriceChangePercentage>
-          <TableData>
-            <PercentageBarContainer>
-              <ValuesContainer>
-                <Value colors={leftSideColors}>
-                  <BulletCircle colors={leftSideColors} />
-                  {currencySymbol}
-                  {formattedNumber(total_volume, '(0.00a)')}
-                </Value>
-                <Value colors={rightSideColors}>
-                  <BulletCircle colors={rightSideColors} />
-                  {currencySymbol}
-                  {formattedNumber(market_cap, '(0.00a)')}
-                </Value>
-              </ValuesContainer>
+    const currencySymbol = getSymbolFromCurrency(props.currency);
+    const minimizedImage = image.replace('large', 'small');
 
-              <PercentageBar colors={rightSideColors}>
-                <PercentageBarFill
-                  colors={leftSideColors}
-                  percentage={calculatePercentage(total_volume, market_cap)}
-                />
-              </PercentageBar>
-            </PercentageBarContainer>
-          </TableData>
-          <TableData>
-            <PercentageBarContainer>
-              <ValuesContainer>
-                <Value colors={leftSideColors}>
-                  <BulletCircle colors={leftSideColors} />
-                  {currencySymbol}
-                  {formattedNumber(circulating_supply, '(0.00a)')}
-                </Value>
-                <Value colors={rightSideColors}>
-                  <BulletCircle colors={rightSideColors} />
-                  {currencySymbol}
-                  {formattedNumber(total_supply, '(0.00a)')}
-                </Value>
-              </ValuesContainer>
-              <PercentageBar colors={rightSideColors}>
-                <PercentageBarFill
-                  colors={leftSideColors}
-                  percentage={calculatePercentage(
-                    circulating_supply,
-                    total_supply
-                  )}
-                />
-              </PercentageBar>
-            </PercentageBarContainer>
-          </TableData>
-          <TableData>
-            <SmallChartContainer>
-              <Charts
-                smallChartData={pricesOfLastSevenDays.price.filter(
-                  (_, i) => i % 24 === 0
-                )}
-                weeklyChanges={weeklyChanges}
-                smallLineChart
+    return (
+      <TableRow
+        key={index}
+        ref={(node) => {
+          props.lastListElementRef(node, observer);
+        }}
+      >
+        <TableData>{index + 1}</TableData>
+        <TableData>
+          <StyledLink to={`coin/${id}`}>
+            <NameContainer>
+              <CoinIcon image={minimizedImage} name={name} />
+              <CoinName>
+                {name}
+                <Symbol>({symbol})</Symbol>
+              </CoinName>
+            </NameContainer>
+          </StyledLink>
+        </TableData>
+        <TableData>
+          {currencySymbol}
+          {current_price}
+        </TableData>
+        <PriceChangePercentage price={hourlyChanges}>
+          <CenterDiv>
+            <ArrowIcon price={hourlyChanges} />
+            {displayPositiveNumber(hourlyChanges)}%
+          </CenterDiv>
+        </PriceChangePercentage>
+        <PriceChangePercentage price={dailyChanges}>
+          <CenterDiv>
+            <ArrowIcon price={dailyChanges} />
+            {displayPositiveNumber(dailyChanges)}%
+          </CenterDiv>
+        </PriceChangePercentage>
+        <PriceChangePercentage price={weeklyChanges}>
+          <CenterDiv>
+            <ArrowIcon price={weeklyChanges} />
+            {displayPositiveNumber(weeklyChanges)}%
+          </CenterDiv>
+        </PriceChangePercentage>
+        <TableData>
+          <PercentageBarContainer>
+            <ValuesContainer>
+              <Value colors={leftSideColors}>
+                <BulletCircle colors={leftSideColors} />
+                {currencySymbol}
+                {formattedNumber(total_volume, '(0.00a)')}
+              </Value>
+              <Value colors={rightSideColors}>
+                <BulletCircle colors={rightSideColors} />
+                {currencySymbol}
+                {formattedNumber(market_cap, '(0.00a)')}
+              </Value>
+            </ValuesContainer>
+
+            <PercentageBar colors={rightSideColors}>
+              <PercentageBarFill
+                colors={leftSideColors}
+                percentage={calculatePercentage(total_volume, market_cap)}
               />
-            </SmallChartContainer>
-          </TableData>
-        </TableRow>
-      );
-    });
-  }
-}
+            </PercentageBar>
+          </PercentageBarContainer>
+        </TableData>
+        <TableData>
+          <PercentageBarContainer>
+            <ValuesContainer>
+              <Value colors={leftSideColors}>
+                <BulletCircle colors={leftSideColors} />
+                {currencySymbol}
+                {formattedNumber(circulating_supply, '(0.00a)')}
+              </Value>
+              <Value colors={rightSideColors}>
+                <BulletCircle colors={rightSideColors} />
+                {currencySymbol}
+                {formattedNumber(total_supply, '(0.00a)')}
+              </Value>
+            </ValuesContainer>
+            <PercentageBar colors={rightSideColors}>
+              <PercentageBarFill
+                colors={leftSideColors}
+                percentage={calculatePercentage(
+                  circulating_supply,
+                  total_supply
+                )}
+              />
+            </PercentageBar>
+          </PercentageBarContainer>
+        </TableData>
+        <TableData>
+          <SmallChartContainer>
+            <Charts
+              smallChartData={pricesOfLastSevenDays.price.filter(
+                (_, i) => i % 24 === 0
+              )}
+              weeklyChanges={weeklyChanges}
+              smallLineChart
+            />
+          </SmallChartContainer>
+        </TableData>
+      </TableRow>
+    );
+  });
+};
